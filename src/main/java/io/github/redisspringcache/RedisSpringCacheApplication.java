e2d57a89cbdafafba3f3550a68a238e93417d5eb
FlavioHenriique <flaviohenrique638@gmail.com>
2022-04-06 13:31:46 -0300

    Adicionando projeto

---
+++ src/main/java/io/github/redisspringcache/RedisSpringCacheApplication.java
@@ -0,0 +1,13 @@
+package io.github.redisspringcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/redisspringcache/RedisSpringCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.redisspringcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
